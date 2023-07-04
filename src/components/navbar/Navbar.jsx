b587522ac92765b77d80ac3033c99ead4b6accd0
John Quiachon <rjohnquia@gmail.com>
2023-07-04 11:12:48 +0800

    cleanup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,10 @@ import { usePathname } from 'next/navigation'
 import Logo from '@/app/icon.png'
 import Image from 'next/image'
 import { useState } from 'react'
+import { FaBars, FaTimesCircle } from "react-icons/fa6";
+
+
+
 
 
 
@@ -35,17 +39,18 @@ const Navbar = () => {
 
 
   const pathname = usePathname();
-  const [menu, setMenu] = useState('false') 
+  const [menu, setMenu] = useState(true) 
+const bar = <FaBars/>;
 
- 
+
 
   return (
-      <div className={styles.container} onClick={()=>{setMenu(!menu)}}> 
+      <div className={styles.container} > 
         {/* LOGO */}
         <div className={styles.logo}>
            <Image src={Logo} width={80} height={80} alt="cpnc" />
            <Link href="/home" > CENTRAL PHILIPPINE <br/> NAZARENE COLLEGE</Link> 
-           <button className={styles.menuBtn}  onClick={()=>{setMenu(!menu)}}>Menu</button>
+           <button className={styles.menuBtn}  onClick={()=>{setMenu(!menu)}}>{!menu ? bar: "X"}</button>
         </div>
 
         {/* MENU */}
@@ -59,6 +64,7 @@ const Navbar = () => {
                   key={link.id}
                   onClick={()=>{setMenu('false')}}
                >
+               
                   {link.title}
                 </Link>
               )
