18efd6f0d6efda313eb4092d68b39329c1b01ebb
John Quiachon <rjohnquia@gmail.com>
2023-07-04 15:16:57 +0800

    add mission statement

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
 
 import styles from './page.module.css'
+import Image from 'next/image'
+import Mission from "@/components/mission/Mission"
+
 
 export default function Home() {
   return (
-    <main className={styles.container}>
 
+    <main className={styles.mainContainer}>
+
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Mentoring Transformational Leaders</h1>
+        <p className={styles.desc}>Central Philippine Nazarene College is committed to mentor transformational leaders of the next generation. We exist to provide quality Christian education, to recognize God's call and to serve the community with excellence. Partner with us in shaping the lives of our learners and equip them for their future.</p>
+        <button className={styles.enrollBtn}>Enroll Now</button>
+      </div>
+      
+      <div className={styles.imageContainer}>
+        <Image
+        src="/hero.png"
+      width={600}
+       height={400}
+        alt='cpnc'
+        className={styles.img}
+        />
+      </div>
+    </div>
+    
+    <Mission />
     </main>
   )
 }
---
+++ src/components/mission/Mission.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './mission.module.css'
+import Image from 'next/image'
+
+const Mission = () => {
+  return (
+    <main className={styles.container} >
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>
+                OUR MISSION
+            </h1>
+
+            <div className={styles.imgContainer}>
+                <Image
+                    src="/mission.svg"
+                    width={50}
+                    height={50}
+                    alt="cpnc qoute"
+                    className={styles.img}
+                />
+                <p className={styles.desc}>
+                    Mentoring every generation of transformational leaders through quality holistic education and exemplary Christ-centered life that influence the church and the global community.
+                </p>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Mission
