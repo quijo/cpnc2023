40882a09188a4f56ed3cdde00712c8396da38c60
John Quiachon <rjohnquia@gmail.com>
2023-07-04 13:01:28 +0800

    fix button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,30 +6,32 @@ import { usePathname } from 'next/navigation'
 import Logo from '@/app/icon.png'
 import Image from 'next/image'
 import { useState } from 'react'
-import { FaBars, FaTimesCircle } from "react-icons/fa6";
 
 
+
+const open = <Image src="/menu.png" width={20} height={20} alt='cpnc' />
+const close = <Image src="/close.png" width={20} height={20} alt='cpnc' />
 
 
 
 
 
 const links = [
-  {
-    id:1,
-    title:"Home",
-    url:"/home"
-  },
-  {
-    id:2,
-    title:"About",
-    url:"/about"
-  },
-  {
-    id:3,
-    title:"Admission",
-    url:"/admission"
-  },
+  // {
+  //   id:1,
+  //   title:"Home",
+  //   url:"/home"
+  // },
+  // {
+  //   id:2,
+  //   title:"About",
+  //   url:"/about"
+  // },
+  // {
+  //   id:3,
+  //   title:"Admission",
+  //   url:"/admission"
+  // },
 
 ]
 
@@ -40,45 +42,53 @@ const Navbar = () => {
 
   const pathname = usePathname();
   const [menu, setMenu] = useState(true) 
-const bar = <FaBars/>;
+ 
+
 
 
 
   return (
-      <div className={styles.container} > 
+    <div className={styles.container} > 
         {/* LOGO */}
-        <div className={styles.logo}>
+      <div className={styles.logo}>
            <Image src={Logo} width={80} height={80} alt="cpnc" />
            <Link href="/home" > CENTRAL PHILIPPINE <br/> NAZARENE COLLEGE</Link> 
-           <button className={styles.menuBtn}  onClick={()=>{setMenu(!menu)}}>{!menu ? bar: "X"}</button>
-        </div>
+      </div>
 
         {/* MENU */}
-        <div className={!menu===true ? styles.activeLinks : styles.links}>
-            {links.map((link) => {
-              const isActive = pathname.startsWith(link.url)
-              return (
-                <Link
-                  className={`${isActive ? 'active' :null} `} 
-                  href={link.url}
-                  key={link.id}
-                  onClick={()=>{setMenu('false')}}
-               >
-               
-                  {link.title}
-                </Link>
-              )
-            })}
-            
-           
-        </div>
+     <div className={styles.navbarMenu}>
+        <Link href="https://vnbc-essentiel.ckgroup.ph/"target='blank'>
+          <button className={styles.loginBtn}>Login</button>
+        </Link>
 
-       
-       
-        
-      </div>
+        <Link href="https://enrollment.vnbc.edu.ph/wp" target='blank'>
+          <button className={styles.enrollBtn}>Enroll Now</button>
+        </Link>    
+     </div>
+              
+    </div>
     
   )
 }
+/*
+ <button className={styles.menuBtn}  onClick={()=>{setMenu(!menu)}}>{!menu ? open: close}</button>
+!menu===true ? styles.activeLinks : 
 
+
+ //   <Link
+              //     className={`${isActive ? 'active' :null} `} 
+              //     href={link.url}
+              //     key={link.id}
+              //  >
+               
+              //     {link.title}
+              //   </Link>
+
+
+                {links.map((link) => {
+              const isActive = pathname.startsWith(link.url)
+              return (
+    )
+            })}
+ */
 export default Navbar
