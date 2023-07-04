4f2770a3eb6ba9449c5d0d306512a237049498df
John Quiachon <rjohnquia@gmail.com>
2023-07-05 00:19:32 +0800

    add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from "@/components/navbar/Navbar"
-import Footer from "@/components/footer/footer"
+import Footer from "@/components/footer/Footer"
 
 
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container} >
+      <div className={styles.copyright}>
+         <p>&copy; 2023 Central Philippine Nazarene College Inc.</p> 
+      </div>
+      <div className={styles.location}>
+        <Image 
+          src="/loc.png"
+          width={30}
+          height={30}
+          />
+          <p>Saint Mary's Drive Apas, Cebu City</p>
+      </div>
+
+      <div className={styles.contact}>
+          <Image 
+          src="/call.png"
+          width={30}
+          height={30}
+          />
+          <p>+62 032 505-5131</p>
+      </div>
+
+      <div className={styles.social}>
+          <Link href="https://www.facebook.com/cpncMain" target='blank'>
+              <Image 
+                src="/facebook.png"
+                width={30}
+                height={30}
+                alt='cpnc facebook'
+              />
+          </Link>
+          <Link href="https://www.facebook.com/cpncMain" target='blank'>
+            <p>CPNC</p> 
+          </Link>
+      </div>
+    </div>
   )
 }
 
