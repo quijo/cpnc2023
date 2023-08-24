454166645b117b7b9dbda1476105239e32b78980
John Quiachon <rjohnquia@gmail.com>
2023-08-24 17:47:07 +0800

    edit buwan ng wika

--- src/app/page.js
+++ src/app/page.js
@@ -13,12 +13,24 @@ export default function Home() {
     <main className={styles.mainContainer}>
 
     <div className={styles.container}>
+      
+      
+      <div className={styles.imageContainer}>
+        <Image
+          src="/buwan.jpg"
+          width={600}
+          height={400}
+          alt='cpnc'
+          className={styles.img}
+        />
+      </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Mentoring Transformational Leaders</h1>
-        <p className={styles.desc}>Central Philippine Nazarene College is committed to mentor transformational leaders of the next generation. We exist to provide quality Christian education, to recognize God&apos;s call and to serve the community with excellence. Partner with us in shaping the lives of our learners and equip them for their future.</p>
-        <h1 className={styles.title}>Welcome Back to School</h1>
+         <h1 className={styles.title}>Central Philippine Nazarene College Celebrates Buwan ng Wika</h1>
+         <h2 className={styles.subTitle}>September 1, 2023</h2>
+        {/*<p className={styles.desc}>Central Philippine Nazarene College is committed to mentor transformational leaders of the next generation. We exist to provide quality Christian education, to recognize God&apos;s call and to serve the community with excellence. Partner with us in shaping the lives of our learners and equip them for their future.</p>
+        <h1 className={styles.title}>Welcome Back to School</h1> */}
         {/* <div className={styles.enrollmentBtn}>
-          <h3>Enroll Now!</h3>
+          
           <Link href="https://vnbc-essentiel.ckgroup.ph/preregv2" target="blank"><button className={styles.btnNew}>New Student</button></Link>
           <Link href="https://vnbc-essentiel.ckgroup.ph/" target="blank"><button className={styles.btnOld}>Old Student</button></Link>
 
@@ -31,16 +43,6 @@ export default function Home() {
         </div> */}
       
                     
-      </div>
-      
-      <div className={styles.imageContainer}>
-        <Image
-          src="/hero4.svg"
-          width={600}
-          height={400}
-          alt='cpnc'
-          className={styles.img}
-        />
       </div>
     </div>
     
