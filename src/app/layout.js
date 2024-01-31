0285e8c7207444a47a1838d65337e01cda1ceb8f
John Quiachon <rjohnquia@gmail.ocm>
2024-01-31 19:42:12 +0800

    try fix push error on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,15 +23,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
           <Navbar />
 
-          <div>
-  
-    </div>
         <div className="container">
           {children}
           
-
-
-        
         </div>
          <Footer /> 
       </body>
